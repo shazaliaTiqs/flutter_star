383fa81f7d3d71f9660fdae28c6f29532b83c7b0
Shaza <shazalia@tiqs.com>
2026-01-28 18:55:04 +0400

    update size

--- lib/src/print_commands.dart
+++ lib/src/print_commands.dart
@@ -202,7 +202,7 @@ class PrintCommands {
         child: repaintBoundary,
       ),
       configuration: ViewConfiguration(
-        size: logicalSize,
+        constraints: BoxConstraints.tight(logicalSize),
         devicePixelRatio: 1.0,
       ),
     );
